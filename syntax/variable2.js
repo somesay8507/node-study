7a7ca4136c64fc48aa5420701ce8cb3ddd53d7bc
somesay8507 <somesay8507@gmail.com>
2022-02-25 17:42:40 +0900

    변수활용 End

--- syntax/variable2.js
+++ syntax/variable2.js
@@ -1,3 +1,4 @@
 var name = 'eibg';
 var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa egoing qui officia deserunt mollit anim id est laborum. '+name;
 console.log(letter);
+//변수를 잘 활용한다면 편하게 코딩작업을 할수 있다.
